92316f7aa964486764bbdfb0a3bb84f76d00a74c
aidarbirzhanov <birzhanov.aydar@gmai.com>
2022-08-23 15:53:20 +0600

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn"
+        alt="header-img"
+      />
+    </header>
+  );
+};
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://thumbs.dreamstime.com/b/cosmos-beauty-deep-space-elements-image-furnished-nasa-science-fiction-art-102581846.jpg"
+          alt=""
+        />
+      </div>
+      <div>ava+descr</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile
